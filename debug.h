1e24bc1dfb8c43b7528f957de59264c833542fd1
Jon Mundall <jon@comcomservices.com>
2016-05-16 15:01:49 -0700

    Initial Upload

---
+++ debug.h
@@ -0,0 +1,25 @@
+#ifndef DEBUG_H
+#define DEBUG_H
+
+#define DEBUG
+
+#ifdef DEBUG
+#define warn(...)  printf(__VA_ARGS__)
+#define error(...) printf(__VA_ARGS__)
+#define debug(...) printf(__VA_ARGS__)
+#endif
+
+#ifdef WARN
+#define warn(...)  printf(__VA_ARGS__)
+#define error(...) printf(__VA_ARGS__)
+#define debug(...) // printf(__VA_ARGS__)
+#endif
+
+#ifdef ERROR
+#define warn(...) // printf(__VA_ARGS__)
+#define error(...) printf(__VA_ARGS__)
+#define debug(...) // printf(__VA_ARGS__)
+#endif
+
+#endif // DEBUG_H
+
